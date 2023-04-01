26f3bf07cdc47936bab7f9cf93fcc1659f18b521
SajjadShafay <shafaysajjad@outlook.com>
2023-04-01 17:39:11 +0100

    Finished

--- IOandStreamsChallenge1/main.cpp
+++ IOandStreamsChallenge1/main.cpp
@@ -1,76 +1,39 @@
 // Section 19
-// Challenge 2
-// Automated Grader
+// Challenge 3
+// Word counter
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <iomanip>
-#include <vector>
-
-struct student {
-    std::string name{};
-    std::string response{};
-    int score{};
-};
+#include <algorithm>
 
 int main() {
 
-    std::ifstream in_file{ "responses.txt" };
-    if (!in_file) {
+    std::ifstream in_file("romeoandjuliet.txt");
+    if (!in_file)
         std::cerr << "File open error" << std::endl;
+
+    std::string line{};
+
+    std::cout << "Please enter a word to find: ";
+    std::string word{}, word_lower{}, word_upper{};
+    std::cin >> word;
+
+    word_upper = word;
+    word_upper[0] = std::toupper(word[0]);
+    word_lower = word;
+    word_lower[0] = std::tolower(word[0]);
+
+    int num_found{}, num_words{};
+
+    while (!in_file.eof()) {
+        in_file >> line;
+        if (line.find(word) != std::string::npos || line.find(word_lower) != std::string::npos || line.find(word_upper) != std::string::npos)
+            num_found++;
+        num_words++;
     }
 
-    std::vector<student> all_students; 
-    int count{ 0 };
-    std::string key{}; 
-    while (!in_file.eof())
-    {
-        if (count == 0) {
-            in_file >> key;
-            count++; 
-        }
-        else {
-            student temp;
-            in_file >> temp.name;
-            in_file >> temp.response;
-            all_students.push_back(temp);
-        }
-    }
+    std::cout << num_words << " words were searched..." << std::endl;
+    std::cout << "The substring " << word << " was found " << num_found << " times" << std::endl;
 
-    const int name_width{ 30 }, response_width{ 24 }, score_width{ 16 };
-
-    std::cout << "1234567890123456789012345678901234567890123456789012345678901234567890" << std::endl;
-    std::cout << "The key is: " << key << std::endl;
-    std::cout << std::endl;
-    std::cout << std::setw(name_width) << std::left << "Name:"
-        << std::setw(response_width) << std::left << "Response:"
-        << std::setw(score_width) << std::right << "Score:" << std::endl;
-    std::cout << "-----------------------------------------------------------------------" << std::endl;
-
-    float average_score{ 0 };
-    for (auto c : all_students)
-    {
-        for (int i = 0; i <= c.response.size()-1 ; i++)
-        {
-            if (c.response.at(i) == key.at(i))
-                c.score++;
-        }
-
-        average_score += c.score; 
-
-        std::cout << std::setw(name_width) << std::left << c.name
-            << std::setw(response_width) << std::left << c.response
-            << std::setw(score_width) << std::right << c.score << std::endl;
-        
-    }
-
-    average_score = average_score / all_students.size();
-    std::cout << "---------------------------------------------------------------------------" << std::endl;
-    std::cout << std::setw(54) << std::left << "Average Score:"
-        << std::setw(16) << std::right << average_score << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Hello world" << std::endl;
     return 0;
 }
-
